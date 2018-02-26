3d922d80763434833376fc97307ab0ce49f90230
XX <13673460778@163.com>
2018-02-27 00:22:17 +0800

    Day4.5

--- Day4/safe.php
+++ Day4/safe.php
@@ -9,9 +9,9 @@
 // sql注入问题：mysql_real_escape_string()已经在7.2中移除，数据库操作应当使用PDO对象（PDOStatement类负责过滤）
 // PDO如何杜绝sql注入问题：http://zhangxugg-163-com.iteye.com/blog/1835721 (从程序间通讯的角度)
 // PDO文档：http://php.net/manual/zh/book.pdo.php
-//
 
-$dsn = 'mysql:dbname=testdb;host=127.0.0.1';
+
+$dsn = 'mysql:dbname=testdb;host=127.0.0.1;port=3306;charset=utf8';
 $user = 'root';
 $password = 'root';
 
@@ -67,4 +67,88 @@ $arr = $preObj->fetchAll(PDO::FETCH_ASSOC);
  * PDO::FETCH_NUM  返回索引数组
  * PDO::FETCH_OBJ  返回由对象组成的二维数组
  */
+
 print_r($arr);
+
+
+
+// Modern PHP 中关于PDO的教程
+$sql = 'SELECT id FROM users WHERE email = :email';
+$statement = $pdo->prepare($sql);
+
+$email = filter_input(INPUT_GET,'email');
+$statement->bindValue(':email',$email,PDO::PARAM_STR); // PDO::PARAM_STR为默认值
+// 其他常用参数 PDO::PARAM_INT PARAM_BOOL PARAM_NULL
+// 预处理语句会自动过滤$email中的值，防止sql注入
+
+$statement->execute();
+while (($result = $statement->fetch()) !== false) {
+    echo $result['email'];
+}
+// PDO::FETCH_ASSOC 将对应结果集中的每一行作为一个由列名索引的数组返回
+// PDO::FETCH_NUM 将对应结果集中的每一行作为一个由列号索引的数组返回
+// PDO::FETCH_BOTH (默认) FETCH_ASSOC与FETCH_NUM的结合
+// PDO::FETCH_OBJ 将结果集中的每一行作为一个属性名对应列名的对象返回
+
+// PDO事务处理
+$money = 50;
+$name1 = "xiaoming";
+$pdo->beginTransaction();//开始事务
+$user1 = $pdo->prepare('UPDATE user 
+                SET amount = amount - :amount
+                WHERE name = :name');
+$user2 = $pdo->prepare('UPDATE user 
+                SET amount = amount - :amount
+                WHERE name = :name');
+
+// bindParam与bindValue的区别 https://segmentfault.com/a/1190000002968592
+$user1->bindParam(':amount',$money);
+$user1->bindValue(':name',$name1);
+$user1->execute();
+$user2->bindValue(':amount',50);
+$user2->bindValue(':name',"zhangsan");
+$user2->execute();
+$pdo->commit();//提交事务
+
+
+
+
+
+// xss攻击
+// xss攻击介绍：http://blog.csdn.net/u011781521/article/details/53894399
+// xss防范讨论：https://segmentfault.com/q/1010000004067521 (智者千虑必有一失)
+// 常用 htmlspecialchars()与htmlentities()
+
+
+$str = "A 'quote' is <b>bold</b>";
+
+// 输出: A 'quote' is &lt;b&gt;bold&lt;/b&gt;
+//echo html_entity_decode(htmlentities($str)),"\n";
+echo htmlentities($str), "\n";
+// 输出: A &#039;quote&#039; is &lt;b&gt;bold&lt;/b&gt;
+echo htmlentities($str, ENT_QUOTES), "\n";
+
+
+$new = htmlspecialchars("<a href='test'>Test</a>", ENT_QUOTES);
+echo $new; // &lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;
+/**
+ * 如果 flags 的设置模糊易混淆，将遵循以下规则：
+ *
+ * 当 ENT_COMPAT、ENT_QUOTES、ENT_NOQUOTES 都没设置， 默认就是 ENT_COMPAT。
+ * 如果设置不止一个 ENT_COMPAT、 ENT_QUOTES、 ENT_NOQUOTES ，优先级最高的是 ENT_QUOTES， 其次是 ENT_COMPAT。
+ * 当 ENT_HTML401、 ENT_HTML5、 ENT_XHTML、 ENT_XML1 都没设置，默认是 ENT_HTML401。
+ * 如果设置不止一个 ENT_HTML401、 ENT_HTML5、 ENT_XHTML、 ENT_XML1， 优先级最高的是 ENT_HTML5 其次是 ENT_XHTML 和 ENT_HTML401。
+ * 如果设置不止一个 ENT_DISALLOWED、 ENT_IGNORE、 ENT_SUBSTITUTE，优先级最高的是 ENT_IGNORE， 其次是 ENT_SUBSTITUTE。
+ */
+
+
+
+
+
+// CSRF攻击
+// 一篇csrf攻击方式与防御策略的文章：http://www.cnblogs.com/hyddd/archive/2009/04/09/1432744.html#!comments
+// 一般框架多采用服务端生成hash并验证的方法防范csrf
+
+
+// DDOS攻击
+// DDOS防御：呵呵,还是砸钱堆带宽或者尝试CDN吧，那不是php该管的事
---
+++ Day5/string.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: XX
+ * Date: 2018/2/27
+ * Time: 0:19
+ */
+
+// php字符处理
+// 讲道理，这东西我一直是查文档的。。。。。
+// TODO 字符处理函数
